76987f027e0a43dc6d4244705e438c2d3bb3a634
seesea2021 <seesea2021@protonmail.com>
2024-05-29 16:38:00 +0800

    feature: update POOL_INIT_CODE_HASH

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x851d77a45b8b9a205fb9f44cb829cceba85282714d2603d601840640628a3da7;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
